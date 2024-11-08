36cea587406711d88a9880df7c17a74aa4cd0ae2
DrSugak <dr.sugak.dev@gmail.com>
2024-11-08 12:16:00 +0200

    migrations from json to redis

--- bicycle_parts_parser.py
+++ bicycle_parts_parser.py
@@ -1,42 +1,27 @@
-import json
 from datetime import datetime
-from pathlib import Path
 from threading import Thread
 
 import lxml
+import redis
 import requests
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 
-Path("data").resolve().mkdir(exist_ok=True)
-
 
 class Olx:
-    def __init__(self, parser: str, user_agent: str) -> None:
+    def __init__(self, db_size: int, html_parser: str, user_agent: str) -> None:
         self.base_url = "https://www.olx.ua"
         self.category_url = [
             "/hobbi-otdyh-i-sport/velo/velozapchasti/",
             "/hobbi-otdyh-i-sport/velo/veloaksessuary/",
         ]
         self.last_part_url = "?currency=UAH&search[order]=created_at:desc&view=list"
-        self.new_data = {}
-        self.json_data = {}
-        self.parser = parser
+        self.html_parser = html_parser
         self.user_agent = {"User-Agent": user_agent}
+        self.send_notice = db_size > 0
 
     def main(self) -> None:
-        self.new_data = {}
-        path = Path("data", "olx.json").resolve()
-        if not path.exists():
-            with path.open(mode="w", encoding="utf-8") as json_file:
-                json.dump({"olx": None}, json_file, indent=4, ensure_ascii=False)
-        with path.open(mode="r", encoding="utf-8") as json_file:
-            self.json_data = json.load(json_file)
         self.parse_all_pages()
-        if len(self.new_data) > 1:
-            with path.open(mode="w", encoding="utf-8") as json_file:
-                json.dump(self.new_data, json_file, indent=4, ensure_ascii=False)
-        self.json_data = {}
 
     def parse_all_pages(self) -> None:
         for category in self.category_url:
@@ -45,7 +30,7 @@ class Olx:
                 page = requests.get(link, headers=self.user_agent, timeout=10)
             except requests.exceptions.RequestException:
                 return
-            soup = BeautifulSoup(page.content, self.parser)
+            soup = BeautifulSoup(page.content, self.html_parser)
             self.find_all_ads_on_page(soup)
             pagination = soup.find_all("li", {"data-testid": "pagination-list-item"})
             last_page = int(pagination[-1].get_text())
@@ -56,7 +41,7 @@ class Olx:
                     page = requests.get(link, headers=self.user_agent, timeout=10)
                 except requests.exceptions.RequestException:
                     return
-                soup = BeautifulSoup(page.content, self.parser)
+                soup = BeautifulSoup(page.content, self.html_parser)
                 self.find_all_ads_on_page(soup)
 
     def find_all_ads_on_page(self, soup: BeautifulSoup) -> None:
@@ -64,7 +49,7 @@ class Olx:
         for ad in ads:
             try:
                 advert = {}
-                advert["name"] = ad.find("h6").get_text().lower()
+                advert["title"] = ad.find("h6").get_text().lower()
                 advert["price"] = ad.find("p", {"data-testid": "ad-price"}).get_text().split("грн")[0].strip()
                 advert["link"] = self.base_url + ad.find("a").get("href")
                 date = ad.find("p", {"data-testid": "location-date"}).get_text().split("-")[-1].strip()
@@ -74,53 +59,41 @@ class Olx:
                 href = advert["link"].split("/uk/")
                 advert["link"] = href[0] + "/" + href[1]
             if "/obyavlenie/" in advert["link"]:
-                if ad["id"] not in self.new_data:
-                    self.new_data[ad["id"]] = advert
-                    if ad["id"] not in self.json_data:
-                        if len(date.split()[0]) != 2:
-                            self.check_ad(advert)
+                if len(date.split()[0]) != 2:
+                    self.check_ad(ad["id"], advert)
 
-    def check_ad(self, advert: dict) -> None:
-        if len(self.json_data) <= 1:
-            return
+    def check_ad(self, _id: str, advert: dict) -> None:
         try:
             page = requests.get(advert["link"], headers=self.user_agent, timeout=10)
         except requests.exceptions.RequestException:
             return
-        soup = BeautifulSoup(page.content, self.parser)
+        soup = BeautifulSoup(page.content, self.html_parser)
         try:
             category = soup.find("ol", {"data-cy": "categories-breadcrumbs"}).find_all("li")[3].find("a").get("href")
         except AttributeError:
             return
         if (self.category_url[0] or self.category_url[1]) in category:
-            print(advert["link"])
+            with redis.Redis(decode_responses=True) as redis_client:
+                exists = redis_client.exists(_id)
+                if not exists:
+                    redis_client.hset(_id, mapping=advert)
+                    if self.send_notice:
+                        redis_client.publish("bicycle", _id)
 
 
 class XT:
-    def __init__(self, parser: str, user_agent: str) -> None:
+    def __init__(self, db_size: int, html_parser: str, user_agent: str) -> None:
         self.base_url = "http://xt.ht/phpbb"
         self.category_url = [
             "http://xt.ht/phpbb/viewforum.php?f=44&price_type_sel=0&sk=t&sd=d&page=all",
             "http://xt.ht/phpbb/viewforum.php?f=83&price_type_sel=0&sk=m&sd=d&page=all",
         ]
-        self.new_data = {}
-        self.json_data = {}
-        self.parser = parser
+        self.html_parser = html_parser
         self.user_agent = {"User-Agent": user_agent}
+        self.send_notice = db_size > 0
 
     def main(self) -> None:
-        self.new_data = {}
-        path = Path("data", "xt.json").resolve()
-        if not path.exists():
-            with path.open(mode="w", encoding="utf-8") as json_file:
-                json.dump({"xt": None}, json_file, indent=4, ensure_ascii=False)
-        with path.open(mode="r", encoding="utf-8") as json_file:
-            self.json_data = json.load(json_file)
         self.parse_all_pages()
-        if len(self.new_data) > 1:
-            with path.open(mode="w", encoding="utf-8") as json_file:
-                json.dump(self.new_data, json_file, indent=4, ensure_ascii=False)
-        self.json_data = {}
 
     def parse_all_pages(self) -> None:
         for category in self.category_url:
@@ -128,7 +101,7 @@ class XT:
                 page = requests.get(category, headers=self.user_agent, timeout=10)
             except requests.exceptions.RequestException:
                 continue
-            soup = BeautifulSoup(page.text, self.parser)
+            soup = BeautifulSoup(page.text, self.html_parser)
             self.find_all_ads_on_page(soup)
 
     def find_all_ads_on_page(self, soup: BeautifulSoup) -> None:
@@ -136,48 +109,40 @@ class XT:
         for ad in ads:
             try:
                 advert = {}
-                advert["name"] = ad.find("a", {"class": "topictitle"}).get_text().lower()
+                advert["title"] = ad.find("a", {"class": "topictitle"}).get_text().lower()
                 advert["price"] = ad.find("span", {"name": "uah_cur"}).get_text().split("грн")[0].strip()
                 advert["link"] = self.base_url + ad.find("a", {"class": "topictitle"}).get("href").split("&sid=")[0][1:]
             except AttributeError:
                 continue
-            self.new_data[advert["link"].split(".php?")[1]] = advert
-            if advert["link"].split(".php?")[1] not in self.json_data:
-                self.check_ad(advert)
+            _id = advert["link"].split(".php?")[1]
+            self.check_ad(_id, advert)
 
-    def check_ad(self, advert: dict) -> None:
-        if len(self.json_data) > 1:
-            print(advert["link"])
+    def check_ad(self, _id: str, advert: dict) -> None:
+        _id = f"xt:{_id}"
+        with redis.Redis(decode_responses=True) as redis_client:
+            exists = redis_client.exists(_id)
+            if not exists:
+                redis_client.hset(_id, mapping=advert)
+                if self.send_notice:
+                    redis_client.publish("bicycle", _id)
 
 
 class XBikers:
-    def __init__(self, parser: str, user_agent: str) -> None:
+    def __init__(self, db_size: int, html_parser: str, user_agent: str) -> None:
         self.base_url = "https://x-bikers.com/board/"
-        self.new_data = {}
-        self.json_data = {}
-        self.parser = parser
+        self.html_parser = html_parser
         self.user_agent = {"User-Agent": user_agent}
+        self.send_notice = db_size > 0
 
     def main(self) -> None:
-        self.new_data = {}
-        path = Path("data", "x-bikers.json").resolve()
-        if not path.exists():
-            with path.open(mode="w", encoding="utf-8") as json_file:
-                json.dump({"x-bikers": None}, json_file, indent=4, ensure_ascii=False)
-        with path.open(mode="r", encoding="utf-8") as json_file:
-            self.json_data = json.load(json_file)
         self.parse_all_pages()
-        if len(self.new_data) > 1:
-            with path.open(mode="w", encoding="utf-8") as json_file:
-                json.dump(self.new_data, json_file, indent=4, ensure_ascii=False)
-        self.json_data = {}
 
     def parse_all_pages(self) -> None:
         try:
             page = requests.get(self.base_url, headers=self.user_agent, timeout=10)
         except requests.exceptions.RequestException:
             return
-        soup = BeautifulSoup(page.content, self.parser)
+        soup = BeautifulSoup(page.content, self.html_parser)
         self.find_all_ads_on_page(soup)
         last_page = soup.find("li", {"class": "last"}).find("a").get("href")
         pagination = int(last_page.split("page=")[1])
@@ -187,7 +152,7 @@ class XBikers:
                 page = requests.get(link, headers=self.user_agent, timeout=10)
             except requests.exceptions.RequestException:
                 continue
-            soup = BeautifulSoup(page.content, self.parser)
+            soup = BeautifulSoup(page.content, self.html_parser)
             self.find_all_ads_on_page(soup)
 
     def find_all_ads_on_page(self, soup: BeautifulSoup) -> None:
@@ -195,28 +160,37 @@ class XBikers:
         for ad in ads:
             try:
                 advert = {}
-                advert["name"] = ad.find("a", {"class": "gb"}).get_text().lower()
+                advert["title"] = ad.find("a", {"class": "gb"}).get_text().lower()
                 advert["price"] = ad.find_all("td", {"class": "bfb"})[2].get_text().split("грн")[0].strip()
                 advert["link"] = ad.find("a", {"class": "gb"}).get("href")
             except AttributeError:
                 continue
-            self.new_data[advert["link"].split("id=")[-1]] = advert
-            if advert["link"].split("id=")[-1] not in self.json_data:
-                self.check_ad(advert)
+            _id = advert["link"].split("id=")[-1]
+            self.check_ad(_id, advert)
 
-    def check_ad(self, advert: dict) -> None:
-        if len(self.json_data) > 1:
-            print(advert["link"])
+    def check_ad(self, _id: str, advert: dict) -> None:
+        _id = f"xbikers:{_id}"
+        with redis.Redis(decode_responses=True) as redis_client:
+            exists = redis_client.exists(_id)
+            if not exists:
+                redis_client.hset(_id, mapping=advert)
+                if self.send_notice:
+                    redis_client.publish("bicycle", _id)
 
 
 def main() -> None:
-    parser = lxml.__name__
-    user_agent = UserAgent()
+    try:
+        with redis.Redis(decode_responses=True) as redis_client:
+            db_size = redis_client.dbsize()
+    except redis.exceptions.ConnectionError:
+        return
+    html_parser = lxml.__name__
     while True:
+        user_agent = UserAgent(platforms="pc").random
         print("Start", datetime.now())
-        olx = Olx(parser, user_agent.random)
-        xt = XT(parser, user_agent.random)
-        x_bikers = XBikers(parser, user_agent.random)
+        olx = Olx(db_size, html_parser, user_agent)
+        xt = XT(db_size, html_parser, user_agent)
+        x_bikers = XBikers(db_size, html_parser, user_agent)
         thread_olx = Thread(target=olx.main)
         thread_xt = Thread(target=xt.main)
         thread_x_bikers = Thread(target=x_bikers.main)
